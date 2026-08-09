f2d84f7118f33436428bf8fabe0a9820d1d57e23
Ken Dawson <ken@kendawson.online>
2026-08-08 22:49:26 -0400

    Update ButtonShowTitleNav.jsx

--- src/components/Navigation/ButtonShowTitleNav.jsx
+++ src/components/Navigation/ButtonShowTitleNav.jsx
@@ -76,58 +76,57 @@ export function ButtonShowTitleNav({ title, category, identifier, desc, start, e
               About this show
             </a>
           </li>
-          <>
-              <li>
-                {isFavorite ? (
-                  <a
-                    className="dropdown-item moreoptions"
-                    href="#"
-                    title={`Remove ${title} from favorites`}
-                    onClick={e => {
-                      e.preventDefault();
-                      removeFromFavorites(title);
-                      triggerToast('Show was removed from Favorites', 'dark');
-                    }}
-                  >
-                    Remove from favorites
-                  </a>
-                ) : (
-                  <a
-                    className="dropdown-item moreoptions"
-                    href="#"
-                    title={`Add ${title} to favorites`}
-                    onClick={e => {
-                      e.preventDefault();
-                      addToFavorites(title);
-                      triggerToast('Show was added to Favorites', 'dark');
-                    }}
-                  >
-                    Add to favorites
-                  </a>
-                )}
-              </li>
-              <li>
-                <a
-                  className="dropdown-item moreoptions text-danger"
-                  href="#"
-                  title={`Report a problem with ${title}`}
-                  onClick={e => { e.preventDefault(); handleReportProblem(); }}
-                >
-                  Report a problem
-                </a>
-              </li>
-              <li>
-                <a
-                  className="dropdown-item moreoptions"
-                  href={`https://archive.org/download/${identifier}`}
-                  title={`Download files for ${title}`}
-                  target="_blank"
-                >
-                  Download files
-                  <img src="/assets/external-link.svg" width="15" className="ms-2 pb-1" title="Opens in a new tab or window" alt="External Link" />
-                </a>
-              </li>
-          </>
+          <li>
+          {isFavorite ? (
+            <a
+              className="dropdown-item moreoptions"
+              href="#"
+              title={`Remove ${title} from favorites`}
+              onClick={e => {
+                e.preventDefault();
+                removeFromFavorites(title);
+                triggerToast('Show was removed from Favorites', 'dark');
+              }}
+            >
+              Remove from favorites
+            </a>
+          ) : (
+            <a
+              className="dropdown-item moreoptions"
+              href="#"
+              title={`Add ${title} to favorites`}
+              onClick={e => {
+                e.preventDefault();
+                addToFavorites(title);
+                triggerToast('Show was added to Favorites', 'dark');
+              }}
+            >
+              Add to favorites
+            </a>
+          )}
+          </li>
+          <li>
+            <a
+              className="dropdown-item moreoptions text-danger"
+              href="#"
+              title={`Report a problem with ${title}`}
+              onClick={e => { e.preventDefault(); handleReportProblem(); }}
+            >
+              Report a problem
+            </a>
+          </li>
+          <li>
+            <a
+              className="dropdown-item moreoptions"
+              href={`https://archive.org/download/${identifier}`}
+              title={`Download files for ${title}`}
+              target="_blank"
+            >
+              Download files
+              <img src="/assets/external-link.svg" width="15" className="ms-2 pb-1" title="Opens in a new tab or window" alt="External Link" />
+            </a>
+          </li>
+
         </ul>
       </div>
     </>
