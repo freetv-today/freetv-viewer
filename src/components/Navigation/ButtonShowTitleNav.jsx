52953fb3a0ec338d13a9f8eb56667e98743f63d3
Ken Dawson <ken@kendawson.online>
2026-08-08 22:59:29 -0400

    Update ButtonShowTitleNav.jsx

--- src/components/Navigation/ButtonShowTitleNav.jsx
+++ src/components/Navigation/ButtonShowTitleNav.jsx
@@ -126,7 +126,6 @@ export function ButtonShowTitleNav({ title, category, identifier, desc, start, e
               <img src="/assets/external-link.svg" width="15" className="ms-2 pb-1" title="Opens in a new tab or window" alt="External Link" />
             </a>
           </li>
-
         </ul>
       </div>
     </>
