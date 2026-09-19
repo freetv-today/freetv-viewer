3cb759815430a581dc666152c1344c0274387051
Ken Dawson <ken@kendawson.online>
2026-09-18 22:10:35 -0400

    bumping cache version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 /** @type {ServiceWorkerGlobalScope} */
 // @ts-nocheck
 /* eslint-env serviceworker */
-const CACHE_NAME = 'freetv-static-v2';
+const CACHE_NAME = 'freetv-static-v3';
 const CACHE_PREFIX = 'freetv-';
 const STATIC_ASSETS = [
   '/',
